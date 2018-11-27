a688fa92da028425edda46475ad8f85e6c6c2891
ulisantillan <ulisantillan@gmail.com>
2018-11-27 07:33:37 -0600

    title,form and weather are all rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Form from "./components/form";
 import Weather from "./components/weather";
+import Titles from "./components/titles";
+
 
 class App extends React.Component{
    render(){
     return(
       <div>
+        <Titles />
         <Form />
         <Weather />
       </div>
---
+++ src/components/titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Crazy Weather</h1>
+                <p> Helps you find weather conditions in cities...  </p>
+            </div>
+        )
+    }
+}
+export default Titles;
