2f7cd5cc95f9a1b6d0f59eb839d29df3e29c1353
ulisantillan <ulisantillan@gmail.com>
2018-11-27 07:19:36 -0600

    rendering the form boxes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./components/form";
+
+class App extends React.Component{
+   render(){
+    return(
+      <div>
+        <Form />
+      </div>
+   )
+  }
+}
+export default App;
