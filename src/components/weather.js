9eb9ed4af066addfca48cd7e7f222694d4d44363
ulisantillan <ulisantillan@gmail.com>
2018-11-27 13:33:22 -0600

    updated css and added pics

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Container } from 'reactstrap';
+import image1 from '../img/location.png';
+import image2 from '../img/temp.png';
+import image3 from '../img/pressure.png';
+import image4 from '../img/wind.png';
+import image5 from '../img/conditions.png';
+import image6 from '../img/humidity.png';
 
 class Weather extends React.Component{
 
@@ -13,6 +19,7 @@ class Weather extends React.Component{
                 {
                     this.props.country && this.props.city && <p className="weather__key">Location:
                         <span className="weather__value">  {this.props.city}, {this.props.country}</span>
+                        <img className="Img" alt='icon' src={image1}/>
                     </p>
                 }
             </Container>
@@ -20,6 +27,7 @@ class Weather extends React.Component{
                 {
                     this.props.temperature && <p className="weather__key">Temperature:
                         <span className="weather__value">  {this.props.temperature}</span>
+                        <img className="Img" alt='icon' src={image2}/>
                     </p>
                 }
             </Container>
@@ -27,6 +35,7 @@ class Weather extends React.Component{
                 {
                     this.props.pressure && <p className="weather__key">Pressure:
                         <span className="weather__value">  {this.props.pressure}</span>
+                        <img className="Img1" alt='icon' src={image3}/>
                     </p>
                 }
             </Container>
@@ -34,6 +43,7 @@ class Weather extends React.Component{
                 {
                     this.props.humidity && <p className="weather__key">Humidity:
                         <span className="weather__value">  {this.props.humidity}</span>
+                        <img className="Img3" alt='icon' src={image6}/>
                     </p>
                 }
             </Container>
@@ -42,6 +52,7 @@ class Weather extends React.Component{
                 {
                     this.props.description && <p className="weather__key">Conditions:
                         <span className="weather__value">  {this.props.description}</span>
+                        <img className="Img2" alt='icon' src={image5}/>
                     </p>
                 }
             </Container>
@@ -49,6 +60,7 @@ class Weather extends React.Component{
                 {
                     this.props.wind && <p className="weather__key">Wind Speed:
                         <span className="weather__value">  {this.props.wind}</span>
+                        <img className="Img1" alt='icon' src={image4}/>
                     </p>
                 }
             </Container>
