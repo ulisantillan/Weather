0b3e08e717b53a9841d97d28c6a6a2598851945d
ulisantillan <ulisantillan@gmail.com>
2018-11-27 07:25:03 -0600

    My weather is rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Form from "./components/form";
+import Weather from "./components/weather";
 
 class App extends React.Component{
    render(){
     return(
       <div>
         <Form />
+        <Weather />
       </div>
    )
   }
---
+++ src/components/weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Weather extends React.Component{
+    render(){
+        return(
+            <div>
+                weather is working
+            </div>
+        )
+    }
+}
+export default Weather;
